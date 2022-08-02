c0182bbaf9df0082d3f1b43a372576610257072d
jibbers42 <797824+jibbers42@users.noreply.github.com>
2022-08-02 18:11:10 -0400

    chore: dart create nucleus_one_exporter

---
+++ bin/nucleus_one_exporter.dart
@@ -0,0 +1,5 @@
+import 'package:nucleus_one_exporter/nucleus_one_exporter.dart' as nucleus_one_exporter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nucleus_one_exporter.calculate()}!');
+}
---
+++ test/nucleus_one_exporter_test.dart
@@ -0,0 +1,8 @@
+import 'package:nucleus_one_exporter/nucleus_one_exporter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
